351ba53818560b2ec207e9e5892a93d7a3749c6d
Alvin <50444758+ZarcDev21@users.noreply.github.com>
2023-07-31 15:59:06 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   port: 3000, //the port for the api to run on.
-  servers: ["https://darvapi.is-an.app"], //the servers for the docs
+  servers: [""], //the servers for the docs
   discord: "https://discord.gg/u8NgtppkRp", //your discord url
 };
  
